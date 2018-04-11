59651ce3cdb160c6adb7acf94ea43364e19bf52f
Alf Eaton <hubgit@users.noreply.github.com>
2018-04-11 15:38:00 +0100

    Fix typings for RxChangeEvent

--- src/typings/rx-change-event.d.ts
+++ src/typings/rx-change-event.d.ts
@@ -1,6 +1,12 @@
 export declare class RxChangeEvent {
     data: {
-        readonly type: 'INSERT' | 'UPDATE' | 'REMOVE';
-    };
+        readonly op: 'INSERT' | 'UPDATE' | 'REMOVE';
+        readonly t: number;
+        readonly db: string;
+        readonly col: string;
+        readonly it: string;
+        readonly doc: object;
+        readonly v: object
+    }
     toJSON(): any;
 }
